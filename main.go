f57b029b57fe9d0e4736edb3007700b9e3a11c2b
Kevin M. Gates <kevinmgates@users.noreply.github.com>
2023-02-09 12:29:18 -0600

    Added simple listener on port 3000

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
-import "fmt"
 
-func main(){
-	
-	fmt.Println("Hello World!")
-	
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	port := ":3000"
+	fmt.Println("Starting server...") // println writes to stdout
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello world!") // Fprint uses the io writer
+	})
+	fmt.Println("Listening on http://localhost" + port)
+	http.ListenAndServe(port, nil)
 }
